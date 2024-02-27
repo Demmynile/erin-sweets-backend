4bef3a9bb38787255f34d9a02188327d3af20374
Demmynile <balogun80@gmail.com>
2024-02-27 04:43:16 +0000

    added some changes

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_URL', 'postgres://store_magk_user:mLhTwypKreFVQDrVcdRRWzcMlZerqsHr@dpg-cnem8run7f5s73e1ouug-a.frankfurt-postgres.render.com/store_magk'),
+      host: env('DATABASE_HOST', 'dpg-cnem8run7f5s73e1ouug-a'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'store_magk'),
       user: env('DATABASE_USERNAME', 'store_magk_user'),
