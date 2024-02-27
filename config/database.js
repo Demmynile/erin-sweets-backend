b7d12f4f84c8ad025081764bfec9b1ab70958f3f
Demmynile <balogun80@gmail.com>
2024-02-27 04:58:41 +0000

    added some changes

--- config/database.js
+++ config/database.js
@@ -2,12 +2,13 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('DATABASE_HOST', 'dpg-cnem8run7f5s73e1ouug-a'),
+      client: env('DATABASE_CLIENT', 'postgres'),
+      host: env('DATABASE_HOST', 'dpg-cnem8run7f5s73e1ouug-a.frankfurt-postgres.render.com'),
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'store_magk'),
       user: env('DATABASE_USERNAME', 'store_magk_user'),
       password: env('DATABASE_PASSWORD', 'mLhTwypKreFVQDrVcdRRWzcMlZerqsHr'),
-      ssl: env.bool('DATABASE_SSL', false),
+      ssl: env.bool('DATABASE_SSL', true),
     },
   },
 });
