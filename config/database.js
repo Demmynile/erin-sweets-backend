e57405c7cd273e1664029594cc57c43836697400
Demmynile <balogun80@gmail.com>
2024-03-12 23:28:55 +0000

    updating the application

--- config/database.js
+++ config/database.js
@@ -1,14 +1,22 @@
+const parse = require("pg-connection-string").parse;
+
+const { host, port, database, user, password } = parse(
+  process.env.DATABASE_URL
+);
+
 module.exports = ({ env }) => ({
+  connection: {  
+    client: 'postgres',
     connection: {
-      client: 'postgres',
-      connection: {
-        client: env('DATABASE_CLIENT', 'postgres'),
-        host: env('DATABASE_HOST', 'dpg-cnem8run7f5s73e1ouug-a.frankfurt-postgres.render.com'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'store_magk'),
-        user: env('DATABASE_USERNAME', 'store_magk_user'),
-        password: env('DATABASE_PASSWORD', 'mLhTwypKreFVQDrVcdRRWzcMlZerqsHr'),
-        ssl: env.bool('DATABASE_SSL', false),
+      host,
+      port,
+      database,
+      user,
+      password,
+      ssl: {
+        rejectUnauthorized: false,
       },
     },
-  });
+      debug: false,
+  },
+});
