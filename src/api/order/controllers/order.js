dd28470d5f31854b07a072c0e95fde93857e3a1e
Demmynile <balogun80@gmail.com>
2024-03-13 15:38:30 +0000

    update the application

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,5 +1,5 @@
 ("use strict");
-const stripe = require("stripe")(process.env.STRIPE_KEY);
+// const stripe = require("stripe")(process.env.STRIPE_KEY);
 /**
  * order controller
  */
@@ -29,20 +29,12 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
         })
       );
 
-      const session = await stripe.checkout.sessions.create({
-        shipping_address_collection: { allowed_countries: ["IN"] },
-        payment_method_types: ["card"],
-        mode: "payment",
-        success_url: process.env.CLIENT_URL + "/success",
-        cancel_url: process.env.CLIENT_URL + "?success=false",
-        line_items: lineItems,
-      });
 
       await strapi
         .service("api::order.order")
-        .create({ data: { products, stripeId: session.id } });
+        .create({ data: { products } });
 
-      return { stripeSession: session };
+      return { data: products };
     } catch (error) {
       ctx.response.status = 500;
       return { error };
