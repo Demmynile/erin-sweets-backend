12f0733f20be5d37a281b23b41f2d281403d6610
Demmynile <balogun80@gmail.com>
2024-03-13 00:24:20 +0000

    updating the application

--- config/plugins.js
+++ config/plugins.js
@@ -1,18 +1,15 @@
-module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      config: {
-        provider: 'cloudinary',
-        providerOptions: {
-          cloud_name: env('CLOUDINARY_NAME'),
-          api_key: env('CLOUDINARY_KEY'),
-          api_secret: env('CLOUDINARY_SECRET'),
-        },
-        actionOptions: {
-          upload: {},
-          delete: {},
-        },
-      },
+module.exports = ({env}) => ({
+  // ...
+  upload: {
+    config: {
+      provider: "strapi-provider-upload-do", 
+      providerOptions: {
+        key: env('DO_SPACE_ACCESS_KEY'),
+        secret: env('DO_SPACE_SECRET_KEY'),
+        endpoint: env('DO_SPACE_ENDPOINT'),
+        space: env('DO_SPACE_BUCKET'),
+      }
     },
-    // ...
-  });
+  }, 
+  // ...
+})
